47c8c1e1c5a55835b7759be34669835f7ae37894
WonChon <arisu1000@gmail.com>
2018-07-27 05:58:28 +0900

    Add app version, client ip

--- main.go
+++ main.go
@@ -3,17 +3,16 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	appVersion := "v0.1"
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 	r.GET("/", func(c *gin.Context) {
-
-		c.JSON(200, gin.H{
+		resp := gin.H{
+			"app version": appVersion,
 			"header": c.Request.Header,
-		})
+			"client ip": c.Request.RemoteAddr,
+		}
+
+		c.JSON(200, resp)
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
