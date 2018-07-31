d2f6a6cc96c9575c3fc76032ff1148fb1fdbbc16
arisu1000 <arisu1000@gmail.com>
2018-07-31 20:43:12 +0900

    Add volume-config function

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"os"
 )
 
-func main() {
-	appVersion := "v0.2"
+const (
+	appVersion = "v0.2"
+)
+
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		resp := gin.H{
@@ -22,5 +27,24 @@ func main() {
 		}
 		c.JSON(200, resp)
 	})
+
+	r.GET("/volume-config", func(c *gin.Context) {
+		filepath := c.Query("path")
+		b, err := ioutil.ReadFile(filepath)
+		if err != nil {
+			fmt.Print(err)
+		}
+		configStr := string(b)
+
+		resp := gin.H{
+			"volume-content": configStr,
+		}
+		c.JSON(200, resp)
+	})
+	return r
+}
+
+func main() {
+	r := setupRouter()
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
